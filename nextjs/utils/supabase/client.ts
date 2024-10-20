6a8a04e5f74a395b4aaf0a24637441c6a321f24a
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-10-20 00:50:50 -0400

    trying to fix auth

--- nextjs/utils/supabase/client.ts
+++ nextjs/utils/supabase/client.ts
@@ -1,8 +1,21 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/ssr';
+import { Database } from '@/types_db';
 
-export function createClient() {
-  return createBrowserClient(
+export const createClient = () =>
+  createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return document.cookie.split('; ').find(row => row.startsWith(name))?.split('=')[1]
+        },
+        set(name: string, value: string, options: { expires: number; path: string; sameSite: 'strict' | 'lax' }) {
+          document.cookie = `${name}=${value}; expires=${new Date(options.expires).toUTCString()}; path=${options.path}; SameSite=${options.sameSite}; Secure`
+        },
+        remove(name: string, options: { path: string }) {
+          document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=${options.path}; SameSite=Lax; Secure`
+        },
+      },
+    }
+  );
